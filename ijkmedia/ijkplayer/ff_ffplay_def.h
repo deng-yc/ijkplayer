9df7e6cab0c557ab71da81e247945c124bc0906a
dengyc <dengyc2013@gmail.com>
2022-04-24 16:27:33 +0800

    Update ff_ffplay_def.h

--- ijkmedia/ijkplayer/ff_ffplay_def.h
+++ ijkmedia/ijkplayer/ff_ffplay_def.h
@@ -893,3 +893,13 @@ inline static const char *ffp_get_error_string(int error) {
 #define MEDIACODEC_MODULE_NAME "MediaCodec"
 
 #endif
+
+// ffplay.c
+//校验是否有必要继续向缓冲区中添加 AVPacket  默认MIN_FRAMES 25 
+static int stream_has_enough_packets(AVStream *st, int stream_id, PacketQueue *queue)
+{
+	return stream_id < 0 ||
+		queue->abort_request ||
+		(st->disposition & AV_DISPOSITION_ATTACHED_PIC) ||
+		queue->nb_packets > MIN_FRAMES && (!queue->duration || av_q2d(st->time_base) * queue->duration > 1.0);
+}
